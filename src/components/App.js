53f8f6997fa07735ac7e6e105e337bb4313c5251
Антон <40547042+a-klimantow@users.noreply.github.com>
2019-08-18 00:51:03 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from "react"
 // import PropTypes from "prop-types";
 
 export default class extends React.Component {
   render() {
-    return <></>;
+    return <>test-sandbox</>
   }
 }
